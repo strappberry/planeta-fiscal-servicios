fe16b36142cdbd770ab59664a43f576ac21615e8
Ivan Aquino <ivnaqn@gmail.com>
2022-02-27 19:03:43 -0600

    PF-17 Se cambia tipo de dato para evitar errores de longitud con razon fiscal muy grande en facturas

---
+++ database/migrations/2022_02_27_184737_cambiar_tipo_dato_nombres_emisor_y_receptor.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('facturas', function (Blueprint $table) {
+            $table->text('nombre_emisor')->change();
+            $table->text('nombre_receptor')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('facturas', function (Blueprint $table) {
+            $table->string('nombre_emisor')->change();
+            $table->string('nombre_receptor')->change();
+        });
+    }
+};
